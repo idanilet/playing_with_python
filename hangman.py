a24434d8526a3d81f861e63e5bc84b505c4d4876
idanilet <78591832+idanilet@users.noreply.github.com>
2023-11-24 16:41:10 +0200

    new script

---
+++ hangman.py
@@ -0,0 +1,51 @@
+import random
+
+
+def choose_word():
+    words = ["python", "java", "javascript", "ruby", "programming", "developer", "computer", "algorithm"]
+    return random.choice(words)
+
+
+def display_word(word, guessed_letters):
+    display = ""  # create an empty string
+    for letter in word:
+        if letter in guessed_letters:
+            display = display + letter + " "
+        else:
+            display = display + "_ "
+    return display
+
+
+def hangman():
+    word_to_guess = choose_word()
+    guessed_letters = []
+    attempts = 6
+
+    print("Welcome to 'Hangman' game!")
+    print(display_word(word_to_guess, guessed_letters))
+
+    while "_" in display_word(word_to_guess, guessed_letters) and attempts > 0:
+        guess = input("Guess a letter: ").lower()
+
+        if guess.isalpha() and len(guess) == 1:
+            if guess in guessed_letters:
+                print("You've already guessed this letter.")
+            elif guess in word_to_guess:
+                guessed_letters.append(guess)
+                print("Correct!")
+            else:
+                guessed_letters.append(guess)
+                attempts -= 1
+                print(f"The letter is not part of the word. You have {attempts} attempts left.")
+        else:
+            print("Please type a valid letter..")
+
+        print(display_word(word_to_guess, guessed_letters))
+
+    if "_" not in display_word(word_to_guess, guessed_letters):
+        print(f"Congratulations, you've guessed the word: {word_to_guess}")
+    else:
+        print(f"You lost. The correct word was: {word_to_guess}")
+
+
+hangman()
